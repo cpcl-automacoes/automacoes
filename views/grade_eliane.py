a5d98246a30a8be53dc8d5470cccb88daf590d06
cpcl-automacoes <cursinhoclaricelispector@usp.br>
2024-09-07 22:32:10 +0000

    Updating files

--- views/grade_eliane.py
+++ views/grade_eliane.py
@@ -5,5 +5,5 @@ from datetime import datetime
 
 st.set_page_config(page_title="Grade Horária - Eliane Potiguara", page_icon="🕒",layout="wide")
 
-sheet_url = st.secrets["spreadsheet_url"]
+sheet_url = st.secrets
 st.write(sheet_url)
