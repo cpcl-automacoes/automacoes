8bb3edd89be1d25f8dee3f079c95ef681f1fc989
cpcl-automacoes <cursinhoclaricelispector@usp.br>
2024-09-07 21:58:14 +0000

    Updating files

--- views/grade_lelia.py
+++ views/grade_lelia.py
@@ -35,4 +35,4 @@ selected_df = selected_df[selected_df.columns[1:]].transpose().reset_index()
 selected_df.columns = ["Horário","Disciplina"]
 selected_df
 
-st.dataframe(selected_df,use_container_width=True)
+st.dataframe(selected_df,use_container_width=True,hide_index=True)
