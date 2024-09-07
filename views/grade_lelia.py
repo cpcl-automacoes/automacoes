f0a9b3bc306fbb813434aba32e23e6589dd5e512
cpcl-automacoes <cursinhoclaricelispector@usp.br>
2024-09-07 21:28:47 +0000

    Updating files

--- views/grade_lelia.py
+++ views/grade_lelia.py
@@ -7,4 +7,22 @@ st.set_page_config(page_title="Grade Horária - Lélia González", page_icon="�
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(worksheet="Turma - LÉLIA GONZÁLEZ")
 
-st.dataframe(df)
+df['Data'] = pd.to_datetime(df['Data'],format="%d/%m/%Y")
+
+# Get today's date
+today = pd.to_datetime(datetime.today().strftime('%d/%m/%Y'))
+
+# Filter rows where the date is equal or greater than today
+filtered_df = df[df['Data'] >= today]
+datas = df["Data"]
+
+menor_data = filtered_df["Data"].min()
+
+data_select = st.selectbox(
+    label="Selecione a data",
+    options = datas,
+)
+
+selected_df = filtered_df.loc[filtered_df["Data"] == data_select]
+
+st.dataframe(selected_df)
