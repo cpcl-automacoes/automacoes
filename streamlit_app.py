a59ca41d1a448059b63196ac722eb24abe1eba4b
cpcl-automacoes <cursinhoclaricelispector@usp.br>
2024-09-07 20:40:06 +0000

    Updating files

---
+++ streamlit_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# --- PAGE SETUP ---
+page_eliane = st.Page(
+    "views/grade_eliane.py",
+    title="Eliane Potiguara",
+    icon=":material/schedule:",
+)
+
+
+# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
+# pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Grade Horária": [page_eliane, google_page]
+    }
+)
+
+
+# --- SHARED ON ALL PAGES ---
+# st.logo("assets/codingisfun_logo.png")
+
+
+# --- RUN NAVIGATION ---
+pg.run()
