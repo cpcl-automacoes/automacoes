cbb463865f6842fb154e32c45129bd01324b4e5b
cpcl-automacoes <cursinhoclaricelispector@usp.br>
2024-09-07 23:06:49 +0000

    Updating files

--- views/grade_eliane.py
+++ views/grade_eliane.py
@@ -24,9 +24,11 @@ datas = filtered_df["Data"]
 
 menor_data = filtered_df["Data"].min()
 
-col1, col2 = st.columns([2,1],vertical_alignment="center,")
+col1, col2 = st.columns([2,1],vertical_alignment="center")
+
 with col1:
     st.markdown("## Grade Horária - Eliane Potiguara")
+
 with col2:
   st.image("assets/logo_com_texto.png",use_column_width="auto")
 
