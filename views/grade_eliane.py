d90c891ae33734b15c8f4cf996090412cf6f45f8
cpcl-automacoes <cursinhoclaricelispector@usp.br>
2024-09-07 22:22:15 +0000

    Updating files

--- views/grade_eliane.py
+++ views/grade_eliane.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
+from datetime import datetime
 
 st.set_page_config(page_title="Grade Horária - Eliane Potiguara", page_icon="🕒",layout="wide")
 
-conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="Turma - ELIANE POTIGUARA")
-
-st.dataframe(df)
+sheet_url = st.secrets["spreadsheet"]
+st.write(sheet_url)
